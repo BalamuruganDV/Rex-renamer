79729fa0229b102a929966c50b626311f4482a7b
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2022-05-26 16:07:36 +0530

    Update ping.py

--- bot/plugins/ping.py
+++ bot/plugins/ping.py
@@ -13,10 +13,7 @@ async def ping_handler(c: Client, m: "types.Message"):
     await add_user_to_database(c, m)
     await c.send_flooded_message(
         chat_id=m.chat.id,
-        text=f"""Hi {name}, I am Rename Bot!\n       
-                I can rename media without downloading it!"
-                Speed depends on your media DC."
-                Just send me media and reply to it with /rename command.""",
+        text=configs.START_TXT.format(name=m.from_user.mention),
         reply_markup=types.InlineKeyboardMarkup([[
            types.InlineKeyboardButton("Show Settings",
                                       callback_data="showSettings")
