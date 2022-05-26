994a05f0705031e9ea296c528b84deefa1546698
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2022-05-26 16:24:44 +0530

    Create start.py

---
+++ bot/plugins/start.py
@@ -0,0 +1,6 @@
+START_TXT = """
+    Hi {name}, I am Rename Bot!   
+    I can rename media without downloading it!"
+    Speed depends on your media DC."
+    Just send me media and reply to it with /rename command.
+    """
