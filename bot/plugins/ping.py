703ddd3b28da28b4bd286cbeca9c5882aa92ec9a
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2022-05-26 16:12:54 +0530

    Update ping.py

--- bot/plugins/ping.py
+++ bot/plugins/ping.py
@@ -5,7 +5,6 @@ from pyrogram import filters
 from pyrogram import types
 from bot.core.db.add import add_user_to_database
 
-name=message.from_user.mention
 @Client.on_message(filters.command(["start", "ping"]) & filters.private & ~filters.edited)
 async def ping_handler(c: Client, m: "types.Message"):
     if not m.from_user:
