9687b36c1f154cb5ca5e0061c1c6edd8aafd9eef
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2022-05-26 14:22:50 +0530

    Update settings.py

--- bot/core/handlers/settings.py
+++ bot/core/handlers/settings.py
@@ -1,5 +1,3 @@
-# (c) @AbirHasan2005
-
 import asyncio
 from pyrogram import types, errors
 from configs import Config
@@ -10,36 +8,36 @@ async def show_settings(m: "types.Message"):
     usr_id = m.chat.id
     user_data = await db.get_user_data(usr_id)
     if not user_data:
-        await m.edit("Failed to fetch your data from database!")
+        await m.edit("Failed to fetch your data from database!.Contact @REX_Bots_Support")
         return
     upload_as_doc = user_data.get("upload_as_doc", False)
     caption = user_data.get("caption", None)
     apply_caption = user_data.get("apply_caption", True)
     thumbnail = user_data.get("thumbnail", None)
     buttons_markup = [
-        [types.InlineKeyboardButton(f"Upload as Doc {'✅' if upload_as_doc else '❌'}",
+        [types.InlineKeyboardButton(f"ᴜᴘʟᴏᴀᴅ ᴀs ᴅᴏᴄ {'✅' if upload_as_doc else '❌'}",
                                     callback_data="triggerUploadMode")],
-        [types.InlineKeyboardButton(f"Apply Caption {'✅' if apply_caption else '❌'}",
+        [types.InlineKeyboardButton(f"ᴀᴘᴘʟʏ ᴄᴀᴘᴛɪᴏɴ {'✅' if apply_caption else '❌'}",
                                     callback_data="triggerApplyCaption")],
-        [types.InlineKeyboardButton(f"Apply Default Caption {'❌' if caption else '✅'}",
+        [types.InlineKeyboardButton(f"ᴀᴘᴘʟʏ ᴅᴇғᴀᴜʟᴛ ᴄᴀᴘᴛɪᴏɴ {'❌' if caption else '✅'}",
                                     callback_data="triggerApplyDefaultCaption")],
-        [types.InlineKeyboardButton("Set Custom Caption",
+        [types.InlineKeyboardButton("sᴇᴛ ᴄᴜsᴛᴏᴍ ᴄᴀᴘᴛɪᴏɴ",
                                     callback_data="setCustomCaption")],
         [types.InlineKeyboardButton(f"{'Change' if thumbnail else 'Set'} Thumbnail",
                                     callback_data="setThumbnail")]
     ]
     if thumbnail:
-        buttons_markup.append([types.InlineKeyboardButton("Show Thumbnail",
+        buttons_markup.append([types.InlineKeyboardButton("sʜᴏᴡ ᴛʜᴜᴍʙɴᴀɪʟ",
                                                           callback_data="showThumbnail")])
     if caption:
-        buttons_markup.append([types.InlineKeyboardButton("Show Caption",
+        buttons_markup.append([types.InlineKeyboardButton("sʜᴏᴡ ᴄᴀᴘᴛɪᴏɴ",
                                                           callback_data="showCaption")])
-    buttons_markup.append([types.InlineKeyboardButton("Close Message",
+    buttons_markup.append([types.InlineKeyboardButton("ᴄʟᴏsᴇ ᴍᴇssᴀɢᴇ",
                                                       callback_data="closeMessage")])
 
     try:
         await m.edit(
-            text="**Here you can setup your settings:**",
+            text="**⚙ Here you can setup your settings:**",
             reply_markup=types.InlineKeyboardMarkup(buttons_markup),
             disable_web_page_preview=True,
             parse_mode="Markdown"
